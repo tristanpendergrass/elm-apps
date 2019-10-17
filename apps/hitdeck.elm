b866f3ccbbfea9f26e47b64567d07f97d549a6a0
tristanpendergrass <tristanpendergrass@gmail.com>
2019-10-16 17:49:44 -0700

    Revert "Refactor card generator to return a random card rather than an array index"
    This reverts commit 1c00521ba5e817333972e2c5322f6e2e03addf4e.

--- apps/hitdeck.elm
+++ apps/hitdeck.elm
@@ -42,7 +42,7 @@ init _ =
 
 type Msg
     = Draw
-    | HandleDrawResult Card
+    | HandleDrawResult Int
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -50,23 +50,25 @@ update msg model =
     case msg of
         Draw ->
             ( model
-            , Random.generate HandleDrawResult generateCard
+            , Random.generate HandleDrawResult (Random.int 0 (Array.length model.deck))
             )
 
         HandleDrawResult result ->
-            ( { model | discard = Array.push result model.discard }, Cmd.none )
+            let
+                drawnCard : Maybe Card
+                drawnCard =
+                    Array.get result model.deck
 
+                newDiscard : Array Card
+                newDiscard =
+                    case drawnCard of
+                        Just card ->
+                            Array.push card model.discard
 
-generateCard : Random.Generator Card
-generateCard =
-    Random.uniform Zero
-        [ One
-        , MinusOne
-        , Two
-        , MinusTwo
-        , Crit
-        , Null
-        ]
+                        Nothing ->
+                            model.discard
+            in
+            ( { model | discard = newDiscard }, Cmd.none )
 
 
 
